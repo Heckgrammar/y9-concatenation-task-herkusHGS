99ab51a981e23c904eb72bd38bfaf60354fff5cb
Herkus Maikstenas <23HMaikstenas@heckgrammar.co.uk>
2026-02-02 09:55:37 +0000

    hi

--- Program.cs
+++ Program.cs
@@ -4,17 +4,21 @@
     {
         static void Main(string[] args)
         {
-            string FirstName;
             Console.Write("Enter first name: ");
-            FirstName = Console.ReadLine();
+            string FirstName = Console.ReadLine();
+            Console.Write("Enter surname: ");
+            string Surname = Console.ReadLine();
+            Console.Write("Enter age: ");
+            string age1 = Console.ReadLine();
+            int age = Convert.ToInt32(age1);
+            Console.WriteLine("Hello, " + FirstName + " " + Surname + ", you are " + age + " years old");
             // add two more variables for the users last name and their age
             // take these inputs into the program
             // use the test data Bob, Smith, 14
             // ammend this code to output in one command
             // Hello Bob Smith you are 14 years old
-            Console.WriteLine(FirstName);
             // save and commit your program
-            // add evidence of the test input and your own input running the program 
+            // add evidence of the test input and your own input running the program
         }
     }
 }
